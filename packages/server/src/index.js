fd34c8488f757dbb15020b36ffd564e61f7fda65
Chris Hutchinson <chris_hutchinson@me.com>
2018-11-28 22:07:22 +0000

    Set up a default renderer, and support passing a flag

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const args = require("args");
 
 const router = require("./router");
-const registerRenderer = require("./renderers");
+const { registerRenderer, validRenderers } = require("./renderers");
 const { resetAndExit } = require("./helpers");
 const { tree, ledCount } = require("./config");
 
 const { initializePixels } = require("./pixels");
 
 const APP_PORT = 2811;
+
+args.option("renderer", "The renderer to use to output the lights", "ws281x");
+
+const flags = args.parse(process.argv);
+console.log(flags.renderer);
+if (!validRenderers.includes(flags.renderer)) {
+  console.error(
+    "Please provide a valid renderer, options:",
+    validRenderers.join(", ")
+  );
+  process.exit(0);
+}
 
 // Setup
 const app = express();
@@ -18,7 +31,7 @@ initializePixels(ledCount);
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(registerRenderer("ws281x", ledCount, tree));
+app.use(registerRenderer(flags.renderer, ledCount, tree));
 app.use("/api/v1", router);
 
 app.listen(APP_PORT, () => console.log(`App listening on port ${APP_PORT}!`));
