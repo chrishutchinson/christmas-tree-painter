2296580db0c8610278ed40c63ce7d0e3d6e285e2
Chris Hutchinson <chris_hutchinson@me.com>
2018-12-16 21:59:18 +0000

    Make the URL scheme part of the hostname

--- packages/client/src/api/index.ts
+++ packages/client/src/api/index.ts
@@ -1,13 +1,13 @@
 import { ColorResult, Mode } from "../types";
 
 export const getTree = (hostname: string) =>
-  fetch(`http://${hostname}/api/v1/tree`).then(res => {
+  fetch(`${hostname}/api/v1/tree`).then(res => {
     if (res.status !== 200) throw new Error(res.statusText);
     return res.json();
   });
 
 export const getPixels = (hostname: string) =>
-  fetch(`http://${hostname}/api/v1/pixels`).then(res => {
+  fetch(`${hostname}/api/v1/pixels`).then(res => {
     if (res.status !== 200) throw new Error(res.statusText);
     return res.json();
   });
@@ -17,7 +17,7 @@ export const setPixelColor = (
   pixelId: number,
   { rgb }: ColorResult
 ) =>
-  fetch(`http://${hostname}/api/v1/pixel/${pixelId}`, {
+  fetch(`${hostname}/api/v1/pixel/${pixelId}`, {
     method: "post",
     body: JSON.stringify({
       color: rgb
@@ -31,7 +31,7 @@ export const setPixelColor = (
   });
 
 export const setMode = (hostname: string, mode: Mode) =>
-  fetch(`http://${hostname}/api/v1/mode/${mode.toLowerCase()}`, {
+  fetch(`${hostname}/api/v1/mode/${mode.toLowerCase()}`, {
     method: "post"
   }).then(res => {
     if (res.status !== 200) throw new Error(res.statusText);
@@ -39,13 +39,13 @@ export const setMode = (hostname: string, mode: Mode) =>
   });
 
 export const getBrightness = (hostname: string) =>
-  fetch(`http://${hostname}/api/v1/pixels/brightness`).then(res => {
+  fetch(`${hostname}/api/v1/pixels/brightness`).then(res => {
     if (res.status !== 200) throw new Error(res.statusText);
     return res.json();
   });
 
 export const setBrightness = (hostname: string, brightness: number) =>
-  fetch(`http://${hostname}/api/v1/pixels/brightness`, {
+  fetch(`${hostname}/api/v1/pixels/brightness`, {
     method: "post",
     body: JSON.stringify({
       brightness
